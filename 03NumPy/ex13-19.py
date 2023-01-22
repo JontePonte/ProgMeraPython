9bd65732716acc074a7256f1ff50be7a58042d8b
JontePonte <john_nordstrand@hotmail.com>
2023-01-22 14:54:36 +0100

    Added files for assignments 13 to 23 in the numpy module

---
+++ 03NumPy/ex13-19.py
@@ -0,0 +1,63 @@
+"My solution to assignment 13 to 19 in the numpy module"
+import numpy as np
+
+#Uppgift 13:
+# Skriv en programrad som raderar 1:a och 3:e elementet nedanstående NumPy-array:
+
+np_1 = np.array([1,2,3,4,5,6])
+
+
+
+#Uppgift 14:
+# Skriv en programrad som raderar 1:a och 2:a raden i nedanstående NumPy-array:
+np_1 = np.array([[1,2,3],
+                 [4,5,6],
+                 [7,8,9]])
+print(np_1)
+
+
+
+
+#Uppgift 15:
+# Skriv en programrad som raderar 2:a kolumnen i nedanstående NumPy-array:
+np_1 = np.array([[1,2,3],
+                 [4,5,6],
+                 [7,8,9]])
+
+
+
+#Uppgift 16:
+# Skriv ett program som reverserar (dvs byter plats på första och sista elementet sist, 
+# andra och näst sista elementet osv) för följande np.array:
+
+np_array =np.array([1,2,3,4,5,6,7,8,9,10])
+
+
+
+#Uppgift 17:
+#Skriv ett program som extraherar första kolumnen i nedanstående NumPy-array och lägger resultatet i en ny NumPy-array.
+
+numpy_array_1 = np.array([[1,2,3],
+                          [4,5,6],
+                          [7,8,9]])
+
+
+
+#Uppgift 18:
+#Skriv ett program som extraherar andra raden i nedanstående NumPy-array och läggerresultatet i en ny NumPy-array.
+
+numpy_array_1 = np.array([[1,2,3],
+                          [4,5,6],
+                          [7,8,9]])
+
+
+
+#Uppgift 19:
+#Skriv ett program som extraherar de två första elementen i första och andra raden i nedanstående NumPy-array och
+# lägger resultatet i en ny NumPy-array.
+
+A = np.array([[1,2,3],
+              [4,5,6],
+              [7,8,9]])
+
+
---
+++ 03NumPy/ex20-23.py
@@ -0,0 +1,42 @@
+"My solutions to assignment 20 to 23 in the numpy module"
+
+import numpy as np
+
+#Uppgift 20:
+#Skriv ett program som extraherar hörnelementen i nedanstående NumPy-array och lägger resultatet i en ny NumPy-array.
+
+A = np.array([[1,2,3],
+              [4,5,6],
+              [7,8,9]])
+
+
+
+#Uppgift 21:
+#Skriv ett program som extraherar diagonalelementen i nedanstående NumPy-array och lägger resultatet i en ny
+#NumPy-array.
+
+A = np.array([[1,2,3],
+              [4,5,6],
+              [7,8,9]])
+
+
+
+#Uppgift 22:
+# Lägg ihop NumPy-arrayerna A och B genom att skapa en ny Nunpy-array 'C' med dimensionen (3,3) och där
+# a. B utgör sista raden i den nya NumPy-array:en 'C'.
+# b. B utgör första raden i den nya NumPy-array:en 'C'.
+
+A = np.array([[1,2,3],
+              [4,5,6]])
+B = np.array([70,80,80])
+
+
+
+#Uppgift 23:
+# Lägg ihop NumPy-arrayerna A och B genom att skapa en ny Nunpy-array 'C' med dimensionen (2,4) och där
+# a. B utgör sista kolumnen i den nya NumPy-array:en 'C'.
+# b. B utgör första kolumnen i den nya NumPy-array:en 'C'.
+
+A = np.array([[1,2,3],
+              [4,5,6]])
+B = np.array([70,80,80])
