e2999feffdae9ce7370e59a12401d0e68c7f85cd
JontePonte <john_nordstrand@hotmail.com>
2023-01-23 11:03:30 +0100

    Created file for assignments 13 to 17 in the second set from numpy module

---
+++ 03NumPy/assignments02/ex13-17.py
@@ -0,0 +1,65 @@
+"My solutions to assignments 13 to 17 in the second assignment list in the numpy module"
+
+import numpy as np
+
+# 13. Skriv en program som skapar nedanstående Numpy-array 
+#     och därfter använder Numpys inbyggda funktioner add(), multiply(), subtract() 
+# och divide() för att beräkna 
+#     summan, produkten, skillnaden och kvoten av A och B.
+A = [[ 1,  2,  3,  4],
+ [ 5,  6,  7,  8],
+ [ 9, 10, 11, 12],
+ [13, 14, 15, 16],]
+
+B = [[11, 12, 13, 14],
+ [15, 16, 17, 18],
+ [19, 20, 21, 22],
+ [23, 24, 25, 26],]
+
+
+
+# 14. Använda Numpy funktion where() för att omvandla följande Numpy-array:
+A = [[1, 2, 3],
+ [4, 5, 6],
+ [7, 8, 9],]
+
+# till Numpy-arrayen:
+# [[ 1  2  3]
+#  [ 4  5 12]
+#  [12 12 12]]
+
+
+
+# 15. Man kan använda  funktionen array_split() för dela en Numpy-array till olika 
+# sub Numpy-arrayer. 
+# Använd denna funktion för att skapa 5 sub Numpy-array från:
+A = [[ 1,  2,  3,  4,  5,  6,  7],
+ [ 8,  9, 10, 11, 12, 13, 14],
+ [15, 16, 17, 18, 19, 20, 21],
+ [22, 23, 24, 25, 26, 27, 28],
+ [29, 30, 31, 32, 33, 34, 35],
+ [36, 37, 38, 39, 40, 41, 42],
+ [43, 44, 45, 46, 47, 48, 49],]
+
+
+
+# 16. Skapa följande Numpy-array
+# A=[ 6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 11, 11, 11, 11, 11]
+# genom att använda funktionen where() från Nympy-arrayen:
+# B=[ 6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26] 
+
+# och därfter hitta positionen för talet 11 i A med hjälp av samma funktion. 
+
+
+
+# 17. Använda  sort() funktionen för att sortera Numpy-array:
+#     ['Röd', 'Grön', 'Vitt', 'Svart','Lilla','Gull', 'violet','Mörkröd','Blå']
+#     och skriv därefter ut positionen för 'Svart' före och efter sorteringen.
+
+
+
+# 18. Skriv ett program som använder funktionerna lcm() och gcd() för att beräkna
+#     minsta gemensamma multipel och största gemensamma nämnare av två tal som du 
+# matar in.
+
+
