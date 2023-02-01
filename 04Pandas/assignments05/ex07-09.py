3a4e0a45b950a67685d0b69ab4bec403340bd7e8
JontePonte <john_nordstrand@hotmail.com>
2023-02-01 19:30:08 +0100

    Solved examples up to 09 in pandas module 05

--- 04Pandas/assignments05/ex07-09.py
+++ 04Pandas/assignments05/ex07-09.py
@@ -9,16 +9,22 @@ temperature_data = pd.read_csv('04Pandas/assignments05/temp_falsterbo_froson.csv
 # Skriv ett program som listar en sammanställning för temepraturen med avseende 
 # på medelvärde-, största- och minsta värden för Falsterbo under juli 2019.
 filt = (temperature_data['Datum'] >= '2019-07-01') & (temperature_data['Datum'] <= '2019-07-31')
-print(temperature_data[filt][['Datum','Falsterbo']].describe())
+# print(temperature_data[filt][['Datum','Falsterbo']].describe())
 
 
 # Uppgift 8:
 # Skriv ett program som listar de dagar under 2019 när temperaturen på Frösön 
 # är högre än +25C
-
+filt = (temperature_data['Froson'] >= 25)
+# print(temperature_data[filt][['Datum', 'Froson']])
 
 
 
 # Uppgift 9:
 # Skriv ett program som listar de dagar under maj, juni, juli, augusti år 2019 
 # när temperaturen på Frösön är högre än temperaturen i Falsterbo.
+filt = (temperature_data['Froson'] >= temperature_data['Falsterbo']) &\
+    (temperature_data['Datum'] >= '2019-06-01') & \
+    (temperature_data['Datum'] <= '2019-08-31')
+
+print(temperature_data[filt])
