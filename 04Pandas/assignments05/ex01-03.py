d089faebc91a76a3b869fc5aba71bd5878c2c999
JontePonte <john_nordstrand@hotmail.com>
2023-02-01 15:42:58 +0100

    Solved the two first assignments in the 05 section of pandas

--- 04Pandas/assignments05/ex01-03.py
+++ 04Pandas/assignments05/ex01-03.py
@@ -1,21 +1,24 @@
 import pandas as pd
 
 
-temperatur_data = pd.read_csv('temp_falsterbo_froson.csv')
+# a little tricky but I remember it from before. 
+# I need to add the folder structure because python is run from the root folder
+temperature_data = pd.read_csv('04Pandas/assignments05/temp_falsterbo_froson.csv')
 
 
 # Uppgift 1:
 #Skriv en programrad som listar antal rader och kolumner hos 'temperatur_data'
-
-
+# print(f"The data has {temperature_data.shape[0]} rows and {temperature_data.shape[1]} columns")
 
 
 # Uppgift 2:
 #Skriv ett program som listar de första 15 raderna och de sista 15 raderna i 'temperatur_data'
-
+# print(temperature_data.iloc[0:15])
+# print(temperature_data.iloc[-15:])
 
 
 # Uppgift 3:
 # Skriv ett program som gör en sammanställning av innehållet i 
 # 'temperatur_data' med avseende på antal data, medelvärde, min- och max- värde
+print(temperature_data.info())
 
