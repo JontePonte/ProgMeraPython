254cc2f60013f6d1166adc625bfa2ba81f9ec742
JontePonte <john_nordstrand@hotmail.com>
2023-02-03 17:09:55 +0100

    Fixed assignments02 and added ex01-04.py solution file

---
+++ 04Pandas/assignments02/ex01-04.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+# Uppgift 1:
+# Skriv en programrad som skapar ett Series-objekt med hjälp av en standard Python lista 
+# innehållande udda heltal från 1 till och med 11.
+
+
+
+
+# Uppgift 2:
+# Skriv ett program som konverterar nedanstående strängindexerade fält 'dict_data' 
+# till ett Series-objekt med namnet 'ds_data'
+
+dict_data = {'a': 1000, 'b': 2000, 'c': 3000, 'd': 4000, 'e': 5000}
+
+
+
+
+# Uppgift 3:
+# Skriv en programrad som skapar ett Series-objekt med hjälp av en NumPy-array 
+# innehållande 6 stycken flyttal mellan 5 och 7.
+
+
+
+
+# Uppgift 4:
+# Skriv ett program som skapar ett Series-objekt 'ds_heltal' från en standard Python 
+# lista 'sp_heltal' innehållande heltalen från 1 t.o.m 6. 
+# Index hos 'ds_heltal' ska vara bokstäverna 'a' - 'f'.
+
+
