63d928ccf45ce48ca9df79ef9ef49c6e5b4dabc3
JontePonte <john_nordstrand@hotmail.com>
2023-02-03 14:15:11 +0100

    Solved all the examples in pandas module 05

--- 04Pandas/assignments05/ex13-14.py
+++ 04Pandas/assignments05/ex13-14.py
@@ -8,7 +8,11 @@ import pandas as pd
 df = pd.DataFrame([[1, 'A'], [2, 'B'],[3, 'A'],[4, 'C'], [3, 'A'],[4, 'D']], 
 columns=['K1', 'K2'])
 
+df['K2'].replace('A', 'Ö', inplace=True)
+df['K1'].replace(4, 40, inplace=True)
+# The ['K2'] and ['K1'] could be removed but I guess it's better practice to leave them in
 
+# print(df)
 
 # Uppgift 14:
 # Skriv ett program som stryker alla rader som innehåller bokstaven 'A' i 
@@ -16,3 +20,6 @@ columns=['K1', 'K2'])
 
 df_1 =pd.DataFrame([[1, 'A'], [2, 'B'],[3, 'A'],[4, 'C'], [3, 'A'],[4, 'D']], 
 columns=['K1', 'K2'])
+
+df_2 = df_1[df_1['K2'] != 'A']
+print(df_2)
